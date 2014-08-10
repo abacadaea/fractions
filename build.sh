85ca5d8e4448f426e5714633dba9d7d1ce82a0b9
EC2 Default User <ec2-user@ip-172-31-46-202.us-west-2.compute.internal>
2014-08-10 14:02:17 +0000

    modified build script

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-export PYTHON_PATH=~/Websites/fractions/src
-./server.py
+mkdir -p /tmp/fractions
+PID_FILE=/tmp/fractions/fractions.pid
+LOG_FILE=/tmp/fractions/fractions.log
+SOCKET=/tmp/fractions/fractions.socket
+
+export PYTHON_PATH=~/Websites/fractions/src:~/fractions/src
+
+uwsgi --stop $PID_FILE
+uwsgi -s $SOCKET --module server --callable app --chmod-socket=666 --pidfile $PID_FILE --logto $LOG_FILE &
